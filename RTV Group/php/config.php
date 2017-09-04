06092ac1364e4e1535dcc1515519d4d7ce9901c1
trofimenko180297 <trofimenko180297@ukr.net>
2017-09-04 11:46:40 +0300

    Update config.php

--- RTV Group/php/config.php
+++ RTV Group/php/config.php
@@ -2,8 +2,8 @@
 require_once ("lib/config.class.php");
 
 Config::set("db.host", "localhost");
-Config::set("db.user", "Vitalik");
-Config::set("db.password", "00vitalik1897");
+Config::set("db.user", "*****");
+Config::set("db.password", "*****");
 Config::set("db.name", "test");
 
 Config::set("salt", "parallax2017");
